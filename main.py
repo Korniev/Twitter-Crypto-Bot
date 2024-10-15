1792500471158c7cb486693904232fe331b15885
Yevhenii Korniev <94477980+Korniev@users.noreply.github.com>
2024-10-15 15:16:14 +0200

    updated for tweepy library
    changed funcionality to working via tweepy Twittet API v2

--- main.py
+++ main.py
@@ -2,17 +2,18 @@ import os
 import json
 import random
 import threading
-
-from schedule import every, run_pending
 import telebot
-import snscrape.modules.twitter as sntwitter
-import requests
+import tweepy
+import schedule
 import time
 from dotenv import load_dotenv
 
 load_dotenv()
 
 bot_token = os.getenv('TELEGRAM_BOT_TOKEN')
+twitter_bearer_token = os.getenv('X_BEARER_TOKEN')
+
+client = tweepy.Client(bearer_token=twitter_bearer_token)
 
 bot = telebot.TeleBot(bot_token)
 
@@ -49,30 +50,32 @@ def twitter_scraper():
     for username in usernames:
         tweets_data = []
         try:
-            # Використовуємо пошук через username
-            query = f"from:{username}"
-            scraper = sntwitter.TwitterSearchScraper(query).get_items()
+            user = client.get_user(username=username)
+            tweets = client.get_users_tweets(user.data.id, max_results=10)
 
-            for i, tweet in enumerate(scraper):
-                if last_tweet_ids.get(username) == tweet.id:
+            for tweet in tweets.data:
+                tweet_id = tweet.id
+                if tweets.data is None:
+                    print(f"No tweets found for {username}")
+                    continue
+
+                if last_tweet_ids.get(username) == tweet_id:
                     break
 
                 data = {
-                    "id": tweet.id,
-                    "url": tweet.url,
+                    "id": tweet_id,
+                    "url": f"https://x.com/{username}/status/{tweet_id}",
                     "username": username
                 }
                 tweets_data.append(data)
 
-                if i == 0:
-                    last_tweet_ids[username] = tweet.id
+                if len(tweets_data) == 0:
+                    last_tweet_ids[username] = tweet_id
 
             all_tweets_data[username] = tweets_data
 
-        except requests.exceptions.SSLError as e:
-            print(f"SSL error while retrieving tweets for {username}: {e}")
-            session = requests.Session()
-            session.get(f'https://x.com/{username}', verify=False)
+        except tweepy.errors.TweepyException as e:
+            print(f"Error fetching tweets for {username}: {e}")
 
     # Save the updated last tweet IDs
     save_last_tweet_ids(last_tweet_ids)
@@ -87,6 +90,7 @@ def send_tweets_to_telegram(tweets_data):
         ads_data = json.loads(ads_file.read())
 
     random_ad = random.choice(ads_data["adsList"])
+    templates = load_templates()
     enabled_groups = load_enabled_groups()
 
     for chat_id, group_info in enabled_groups.items():
@@ -97,8 +101,9 @@ def send_tweets_to_telegram(tweets_data):
 
             for username, profiles in tweets_data.items():
                 for profile in profiles:
-                    url = profile['url']
-                    message += f"[{profile['username']}]({url}) || "
+                    if 'id' in profile and 'username' in profile:
+                        url = profile['url']
+                        message += f"[{profile['username']}]({url}) || "
 
             message = message.rstrip(" || ")
 
@@ -133,22 +138,51 @@ def save_influencers(influencers):
         json.dump(influencers, file)
 
 
-def load_message_ids():
+def load_templates():
     try:
-        with open(message_ids_file, 'r') as file:
+        with open(templates_file, 'r') as file:
             return json.load(file)
     except (FileNotFoundError, json.JSONDecodeError):
         return {}
 
 
-def save_message_ids(message_ids):
-    with open(message_ids_file, 'w') as file:
-        json.dump(message_ids, file)
+def save_templates(templates):
+    with open(templates_file, 'w') as file:
+        json.dump(templates, file)
 
 
-def get_saved_message_id(chat_id):
-    message_ids = load_message_ids()
-    return message_ids.get(str(chat_id))
+def load_admins():
+    try:
+        with open(admins_file, 'r') as file:
+            return json.load(file)
+    except (FileNotFoundError, json.JSONDecodeError):
+        return {}
+
+
+admins = load_admins()
+
+
+def load_selected_group():
+    try:
+        with open('selected_groups.json', 'r') as file:
+            return json.load(file)
+    except (FileNotFoundError, json.JSONDecodeError):
+        return {}
+
+
+selected_groups = load_selected_group()
+
+
+def save_admins(admins):
+    with open(admins_file, 'w') as file:
+        json.dump(admins, file)
+
+
+def is_group_admin(message: telebot.types.Message):
+    user_id = message.from_user.id
+    chat_member = bot.get_chat_member(message.chat.id, user_id)
+    if chat_member.status in ['administrator', 'creator']:
+        return True
 
 
 def send_message_with_link(chat_id, message):
@@ -169,11 +203,29 @@ def save_message_id(chat_id, message_id):
     save_message_ids(message_ids)
 
 
+def get_saved_message_id(chat_id):
+    message_ids = load_message_ids()
+    return message_ids.get(str(chat_id))
+
+
+def load_message_ids():
+    try:
+        with open(message_ids_file, 'r') as file:
+            return json.load(file)
+    except (FileNotFoundError, json.JSONDecodeError):
+        return {}
+
+
+def save_message_ids(message_ids):
+    with open(message_ids_file, 'w') as file:
+        json.dump(message_ids, file)
+
+
 # Function to run the Twitter scraper repeatedly
 def run_twitter_scraper():
-    every(1).minute.do(twitter_scraper)
+    schedule.every(1).minute.do(twitter_scraper)
     while True:
-        run_pending()
+        schedule.run_pending()
         time.sleep(1)
 
 
